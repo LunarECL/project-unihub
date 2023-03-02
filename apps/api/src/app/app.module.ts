a675d2d9f16bbba6d230e6a0170e8669eb45a9a3
LunarECL <jkjk98765@naver.com>
2023-03-02 01:03:46 -0500

    feat(api): add joi for env validation and @nestjs/typeorm, mysql2, @nestjs/config for db connection and configuration
    - Added joi package to validate the environment variables but added a TODO comment to update it later to block empty values
    - Added @nestjs/typeorm package for connecting to the database with Nest.js
    - Added mysql2 package for MySQL database driver
    - Added @nestjs/config package for loading the database information from the environment file

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
+// import Joi from "joi";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      // validationSchema: Joi.object({
+      //   NOED_ENV: Joi.string().valid('dev', 'prod').required(),
+      //   DB_HOST: Joi.string().required(),
+      //   DB_PORT: Joi.string().required(),
+      //   DB_USERNAME: Joi.string().required(),
+      //   DB_PASSWORD: Joi.string().required(),
+      //   DB_NAME: Joi.string().required(),
+      // }),
+      // TODO: modify later to module to block the empty value
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
