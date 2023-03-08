77cb03fbbaf0a7ba298270475735cada85db85bc
LunarECL <jkjk98765@naver.com>
2023-03-08 02:31:52 -0500

    Refactor(webapp): Implement protected-route.tsx to restrict unauthorized access to sharedoc page

--- apps/webapp/src/app/app.tsx
+++ apps/webapp/src/app/app.tsx
@@ -3,13 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import { Landing } from '@unihub/webapp/public';
 import { Sharedoc } from '@unihub/webapp/sharedoc';
 import { ThemeProviderWrapper } from './ThemeProviderWrapper';
+import ProtectedRoute from '../../../../libs/webapp/ui/src/lib/protected-route/protected-route';
 
 export function App() {
   return (
     <ThemeProviderWrapper>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/sharedDocument" element={<Sharedoc />} />
+        <Route
+          path="/sharedDocument"
+          element={<ProtectedRoute component={Sharedoc} />}
+        />
       </Routes>
     </ThemeProviderWrapper>
   );
---
+++ libs/webapp/ui/src/lib/protected-route/protected-route.tsx
@@ -0,0 +1,12 @@
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+
+export interface ProtectedRouteProps {
+  component: React.ComponentType;
+}
+
+export function ProtectedRoute({ component, ...options }: ProtectedRouteProps) {
+  const Component = withAuthenticationRequired(component, options);
+  return <Component />;
+}
+
+export default ProtectedRoute;
