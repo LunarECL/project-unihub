845606a09e673db019d71659d01bd06ae784409f
Nicole Droi <nicoledroi@MacBook-Air.local>
2023-03-07 22:25:35 -0500

    feat: sharedoc removed multiple Quill toolbars (Quill bug)

--- libs/webapp/sharedoc/src/lib/sharedoc.tsx
+++ libs/webapp/sharedoc/src/lib/sharedoc.tsx
@@ -1,23 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 
-export interface SharedocProps { }
+export interface SharedocProps {}
 
 import ReconnectingWebSocket from 'reconnecting-websocket';
 import * as  ShareDB from 'sharedb/lib/client';
 //@ts-ignore
-import StringBinding from 'sharedb-string-binding';
-// var richText = require('rich-text');
-//@ts-ignore
 import * as richText from 'rich-text';
-// var Quill = require('quill');
 //@ts-ignore
 import * as Quill from 'quill';
 ShareDB.types.register(richText.type);
 import 'quill/dist/quill.snow.css';
 
 export function Sharedoc(props: SharedocProps) {
-
-  const [status, setStatus] = useState('Not Connected');
   
   useEffect(() => { 
     
@@ -25,35 +19,24 @@ export function Sharedoc(props: SharedocProps) {
     
     var socket = new ReconnectingWebSocket('ws://localhost:3030');
     const connection = new ShareDB.Connection(socket as any);
-    
-    socket.addEventListener('open', function (event) {
-      setStatus('Connected');
-    }
-    );
-
-    socket.addEventListener('close', function (event) {
-      setStatus('Not Connected');
-    }
-    );
-
-    socket.addEventListener('error', function (event) {
-      setStatus('Error');
-    }
-    );
 
     const doc  = connection.get('examples', 'textarea');
     doc.subscribe(function(err: any) {
       if (err) throw err;
-      console.log(doc.data);
-      var quill = new Quill('#editor', {theme: 'snow'});
+
+      var quill = new Quill('#editor', {theme: 'snow',});
+      
+      //Removing the first toolbar so only one shows up (Quill bug)
+      (document.querySelector('.ql-toolbar') as HTMLElement).style.display = 'none';
+
       quill.setContents(doc.data);
+
       quill.on('text-change', function(delta: any, oldDelta: any, source: any) {
         if (source !== 'user') return;
-        console.log('delta', delta);
         const currentText = doc.data.content;
         doc.submitOp(delta, {source: quill});
-        //
       });
+
       doc.on('op', function(op, source) {
         if (source === quill) return;
         quill.updateContents(op);
