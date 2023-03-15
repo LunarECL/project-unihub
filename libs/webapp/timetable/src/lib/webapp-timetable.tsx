594fae18b7dfea2af643a634995f47554002cdeb
Nicole Droi <nicoledroi@MacBook-Air.local>
2023-03-15 14:36:43 -0400

    feat: time table UI changes

--- libs/webapp/timetable/src/lib/webapp-timetable.tsx
+++ libs/webapp/timetable/src/lib/webapp-timetable.tsx
@@ -1,5 +1,5 @@
 import styles from './webapp-timetable.module.css';
-import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Grid, AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 
 /* eslint-disable-next-line */
 export interface WebappTimetableProps {}
@@ -36,37 +36,39 @@ const rows = [
 
 export function WebappTimetable(props: WebappTimetableProps) {
   return (
-    <TableContainer sx={{marginLeft: '5%', marginRight: '10%', marginTop: '2%'}}>
-      <Table sx={{ maxWidth: '80%', maxHeight: '80%'}} aria-label="simple table">
-        <TableHead>
-          <TableRow sx={{fontWeight: 'medium'}}>
-            <TableCell>Time</TableCell>
-            <TableCell align="right">Monday</TableCell>
-            <TableCell align="right">Tuesday</TableCell>
-            <TableCell align="right">Wednesday</TableCell>
-            <TableCell align="right">Thursday</TableCell>
-            <TableCell align="right">Friday</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.time}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.time}
-              </TableCell>
-              <TableCell align="right">{row.monday}</TableCell>
-              <TableCell align="right">{row.tuesday}</TableCell>
-              <TableCell align="right">{row.wednesday}</TableCell>
-              <TableCell align="right">{row.thursday}</TableCell>
-              <TableCell align="right">{row.friday}</TableCell>
+    <>
+    <TableContainer sx={{ marginLeft: '5%', marginRight: '10%', marginTop: '2%' }}>
+        <Table sx={{ maxWidth: '80%', maxHeight: '80%' }} aria-label="simple table">
+          <TableHead>
+            <TableRow sx={{ fontWeight: 'medium' }}>
+              <TableCell>Time</TableCell>
+              <TableCell align="right">Monday</TableCell>
+              <TableCell align="right">Tuesday</TableCell>
+              <TableCell align="right">Wednesday</TableCell>
+              <TableCell align="right">Thursday</TableCell>
+              <TableCell align="right">Friday</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.time}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.time}
+                </TableCell>
+                <TableCell align="right">{row.monday}</TableCell>
+                <TableCell align="right">{row.tuesday}</TableCell>
+                <TableCell align="right">{row.wednesday}</TableCell>
+                <TableCell align="right">{row.thursday}</TableCell>
+                <TableCell align="right">{row.friday}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
